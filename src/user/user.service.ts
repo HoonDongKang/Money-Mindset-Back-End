872a6874c1d95d44195ed6471f7045516e69fbfe
HoonDongKang <d159123@naver.com>
2023-03-02 22:53:29 +0900

    /test

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
+//test
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
