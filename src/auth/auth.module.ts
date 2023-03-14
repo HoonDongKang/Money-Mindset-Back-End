f2b6badbe5b5f54b025dcec87c12ac5b43a3cdae
HoonDongKang <d159123@naver.com>
2023-03-14 23:55:28 +0900

    BLD: creating jwt

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from './../user/user.module';
+import { AuthService } from './auth.service';
+import { PrismaService } from './../prisma/prisma.service';
+
+@Module({
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.NODEE,
+    }),
+  ],
+  providers: [AuthService, PrismaService],
+  exports: [AuthService],
+})
+export class AuthModule {}
