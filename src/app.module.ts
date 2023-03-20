557bf53cdeb8e3a0ab39f3912a7f20d14994bea2
HoonDongKang <d159123@naver.com>
2023-03-20 20:48:44 +0900

    //

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
-import { AuthService } from './auth/auth.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
-import e from 'express';
-import { JwtService } from '@nestjs/jwt';
+
 @Module({
   imports: [
     UserModule,
