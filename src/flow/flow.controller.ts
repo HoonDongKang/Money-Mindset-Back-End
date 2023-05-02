22ed4472679dd91d5e0815fd67af72cc83170c01
HoonDongKang <d159123@naver.com>
2023-05-02 22:58:55 +0900

    ENH: create update flow, delete flow

--- src/flow/flow.controller.ts
+++ src/flow/flow.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   ParseIntPipe,
+  Patch,
   Post,
   Query,
 } from '@nestjs/common';
@@ -12,6 +14,7 @@ import { PrismaService } from '../prisma/prisma.service';
 import { FlowService } from './flow.service';
 import { flowCategory } from './flowCategory';
 import { CreateFlowDto } from './dto/create-flow.dto';
+import { UpdateFlowDto } from './dto/update-flow.dto';
 
 @ApiTags('flow')
 @Controller('flow')
@@ -26,7 +29,7 @@ export class FlowController {
     return flowCategory;
   }
 
-  @Get('/:user_idx')
+  @Get('user/:user_idx')
   // param : userIdx, Date
   // 해당 월의 데이터만 출력
   getTest(
@@ -38,11 +41,24 @@ export class FlowController {
     return this.FlowService.getUserflows(user_idx, start_date, end_date);
   }
 
-  @Post('/:user_idx')
+  @Post('user/:user_idx')
   createUserFlow(
     @Param('user_idx', ParseIntPipe) user_idx: number,
     @Body() createFlowDto: CreateFlowDto,
   ) {
     return this.FlowService.createFlow(user_idx, createFlowDto);
+  }
+
+  @Patch('/:idx')
+  updateUserFlow(
+    @Param('idx', ParseIntPipe) idx: number,
+    @Body() updateFlowDto: UpdateFlowDto,
+  ) {
+    return this.FlowService.updateFlow(idx, updateFlowDto);
+  }
+
+  @Delete('/:idx')
+  deleteUserFlow(@Param('idx', ParseIntPipe) idx: number) {
+    return this.FlowService.deleteFlow(idx);
   }
 }
--- src/flow/flow.service.ts
+++ src/flow/flow.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { flowCategory } from './flowCategory';
 import { CreateFlowDto } from './dto/create-flow.dto';
+import { UpdateFlowDto } from './dto/update-flow.dto';
 
 @Injectable()
 export class FlowService {
@@ -10,21 +11,39 @@ export class FlowService {
   flowIdtoName(flowArr: any) {
     for (const category of flowCategory) {
       for (const flow of flowArr) {
-        if (category.id === flow.id) {
-          Object.assign(flow, { name: category.name });
+        if (category.id === flow.flow_id) {
+          Object.assign(flow, { flowName: category.name });
         }
       }
     }
     return flowArr;
   }
 
+  async findByIdx(idx: number) {
+    const flow = await this.prisma.flow.findFirst({
+      where: { idx },
+      include: {
+        user: {
+          select: {
+            email: true,
+            nickname: true,
+          },
+        },
+      },
+    });
+    if (!flow) {
+      throw new NotFoundException(`Flow number ${idx} doesn't exist.`);
+    }
+  }
+
   async getUserflows(user_idx: number, start_date: number, end_date: number) {
-    return await this.prisma.flow.findMany({
+    const userFlows = await this.prisma.flow.findMany({
       where: {
         user_idx,
         flow_date: { gte: new Date(start_date), lte: new Date(end_date) },
       },
     });
+    return this.flowIdtoName(userFlows);
   }
 
   async createFlow(user_idx: number, createFlowDto: CreateFlowDto) {
@@ -41,9 +60,23 @@ export class FlowService {
     });
   }
 
-  async updateFlow() {}
+  async updateFlow(idx: number, updateFlowDto: UpdateFlowDto) {
+    await this.findByIdx(idx);
+    if (updateFlowDto.flow_date) {
+      updateFlowDto.flow_date = new Date(updateFlowDto.flow_date);
+    }
+    return this.prisma.flow.update({
+      where: { idx },
+      data: updateFlowDto,
+    });
+  }
 
-  async deleteFlow() {}
+  async deleteFlow(idx: number) {
+    await this.findByIdx(idx);
+    return this.prisma.flow.delete({
+      where: { idx },
+    });
+  }
 
   getAllFlows() {
     // return this.prisma.flow.find;
