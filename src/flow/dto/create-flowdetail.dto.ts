1a4d2292e59c75f0adaf7d479cc6679eb1c30e9a
HoonDongKang <d159123@naver.com>
2023-07-06 13:20:51 +0900

    ENH: change flow_detail schema

--- src/flow/dto/create-flowdetail.dto.ts
+++ src/flow/dto/create-flowdetail.dto.ts
@@ -1,16 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class FlowDetailData {
   @ApiProperty({ name: 'detail', example: 'April' })
+  @IsOptional()
   @IsString()
   detail: string;
 
   @ApiProperty({ name: 'lat', example: 35.891374797181705 })
+  @IsOptional()
   @IsNumber()
   lat: number;
 
   @ApiProperty({ name: 'lng', example: 128.6151231834401 })
+  @IsOptional()
   @IsNumber()
   lng: number;
 }
